cbb381f72c0657d9403d939e854a7115cfd96873
Benjamin Hand <gneeshot@gmail.com>
2015-07-07 12:31:40 -0500

    added scope in routes, few methods in user controller for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :forums
-  devise_for :users
+  devise_for :users, :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks"
+    }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   root to: 'forums#index'
 end
