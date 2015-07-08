b06bd58dbbbd0519d1bc722fec66e4005919dfa8
Benjamin Hand <gneeshot@gmail.com>
2015-07-08 16:13:40 -0500

    moved github profile create method

--- app/models/github_profile.rb
+++ app/models/github_profile.rb
@@ -1,16 +1,3 @@
 class GithubProfile < ActiveRecord::Base
   belongs_to :user
-
-  def self.create_github_profile(auth)
-      GithubProfile.create({
-        user_id: current_user.id,
-        hireable: auth[:extra][:raw_info][:hireable],
-        public_repos: auth[:extra][:raw_info][:public_repos]||"",
-        public_gists: auth[:extra][:raw_info][:public_gists]||"",
-        followers: auth[:extra][:raw_info][:followers]||"",
-        following: auth[:extra][:raw_info][:following]||"",
-        created: auth[:extra][:raw_info][:created_at]||"",
-        access_token: auth[:credentials][:token]})
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,34 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :forums
   has_many :comments
-  has_many :github_profiles
+  has_one :github_profiles
 
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email || auth.extra.raw_info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.image = auth.info.image
-      user.github_url = auth.info.urls.github
-      user.blog = auth.info.urls.blog
-      user.location = auth.raw_info.location
-      user.bio = auth.raw_info.bio
-      self.create_github_profile(auth)
+      user.github_url = auth.info.urls.GitHub
+      user.blog = auth.info.urls.Blog || ""
+      user.location = auth.extra.raw_info.location || ""
+      user.bio = auth.extra.raw_info.bio || ""
+      user.save!
+      user.create_github_profile(auth)
     end
+  end
+
+  def create_github_profile(auth)
+      GithubProfile.create({
+        user_id:      id,
+        hireable:     auth[:extra][:raw_info][:hireable],
+        public_repos: auth[:extra][:raw_info][:public_repos]||"",
+        public_gists: auth[:extra][:raw_info][:public_gists]||"",
+        followers:    auth[:extra][:raw_info][:followers]||"",
+        following:    auth[:extra][:raw_info][:following]||"",
+        created:      auth[:extra][:raw_info][:created_at]||"",
+        access_token: auth[:credentials][:token]})
   end
 
   def self.new_with_session(params, session)
