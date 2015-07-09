0507285006c554f85c7a9c03cc6758e9d6c752d6
Benjamin Hand <gneeshot@gmail.com>
2015-07-09 15:57:17 -0500

    repos being displayed on profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,18 @@ class UsersController < ApplicationController
     client = Octokit::Client.new(access_token: @github_profile[:access_token])
     @repos = []
     client.repos.each do |repo|
-      @repos << { name: repo.name,
+      @repos << {
+                name: repo.name,
          description: repo.description,
                  url: repo.html_url,
             language: repo.language,
              created: repo.created_at,
              updated: repo.pushed_at,
                stars: repo.stargazers_count,
-               forks: repo.forks_count,
-            watchers: repo.watchers_count }
+               forks: repo.forks,
+            watchers: repo.watchers }
     end
-    render json: client.repos
-
   end
+
 
 end
