e13dddf6187d467f327e8f017cfdf59c0db0d299
Benjamin Hand <gneeshot@gmail.com>
2015-07-10 15:55:30 -0500

    working on profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,7 @@
 //= require components
 //= require materialize-sprockets
 //= require_tree .
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
