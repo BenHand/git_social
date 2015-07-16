1aa5f0f04ca4b1f805942b6c35242e044b27acfd
Benjamin Hand <gneeshot@gmail.com>
2015-07-16 11:13:38 -0500

    trying to fix omniauth on mobile bug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_git_social_session'
+Rails.application.config.session_store :cookie_store, key: '_git_social_session',
+                                        domain: 'git-social.herokuapp.com'
