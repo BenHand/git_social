7cd4b16fb199f6513d09e4f144cbf345ab6f4b45
Benjamin Hand <gneeshot@gmail.com>
2015-07-16 20:14:54 -0500

    working on markdown for comments/posts, fixed issue with users not having public email on github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,24 @@
 module ApplicationHelper
-  module ApplicationHelper
   def markdown(content)
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
       autolink: true,
       space_after_headers: true,
       fenced_code_blocks: true,
+      gh_blockcode: true,
       underline: true,
       highlight: true,
       footnotes: true,
-      tables: true
+      no_intra_emphasis: true,
     })
-    @markdown.render(content)
+    syntax_highlighter(@markdown.render(content)).html_safe
   end
-end
+
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML(html)
+    doc.search("//pre[@lang]").each do |pre|
+      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
+    end
+    doc.to_s
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email || auth.extra.raw_info.email
+      user.email = auth.info.email || auth.extra.raw_info.email ||
+                   "changeme#{Faker::Number.number(6)}@example.com"
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.image = auth.info.image
