be6ccd8cccb6f957bc2bd22c9c54cbb370bee3e5
Benjamin Hand <gneeshot@gmail.com>
2015-07-17 14:21:59 -0500

    removing complexity from user model (somewhat)

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,30 +15,37 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email || "changeme#{rand(1..1000000)}@example.com"
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.image = auth.info.image
-      user.github_url = auth.info.urls.GitHub
-      user.blog = auth.info.urls.Blog || ""
-      user.location = auth.extra.raw_info.location || ""
-      user.bio = auth.extra.raw_info.bio || ""
+      # TODO: remove hackyness of email hack to account for possible private emails on github
+      user.email      = auth.info.fetch(email, "changeme#{rand(1..1000000)}@example.com")
+      user.password   = Devise.friendly_token[0,20]
+      user.name       = auth.info.fetch(name, "")
+      user.image      = auth.info.fetch(image, "https://38.media.tumblr.com/avatar_951b22788b15_128.png")
+      user.github_url = auth.info.urls.fetch(GitHub, "")
+      user.blog       = auth.info.urls.fetch(Blog, "")
+      user.location   = auth.extra.raw_info.fetch(location, "")
+      user.bio        = auth.extra.raw_info.fetch(bio, "")
       user.save!
       user.create_github_profile(auth)
     end
   end
 
   def create_github_profile(auth)
-      GithubProfile.create({
+    @auth = auth
+    GithubProfile.create({
         user_id:      id,
-        hireable:     auth[:extra][:raw_info][:hireable],
-        public_repos: auth[:extra][:raw_info][:public_repos]||"",
-        public_gists: auth[:extra][:raw_info][:public_gists]||"",
-        followers:    auth[:extra][:raw_info][:followers]||"",
-        following:    auth[:extra][:raw_info][:following]||"",
-        created:      auth[:extra][:raw_info][:created_at]||"",
-        access_token: auth[:credentials][:token]})
+        hireable:     auth_raw_info(:hireable),
+        public_repos: auth_raw_info(:public_repos),
+        public_gists: auth_raw_info(:public_gists),
+        followers:    auth_raw_info(:followers),
+        following:    auth_raw_info(:following),
+        created:      auth_raw_info(:created_at),
+        access_token: auth[:credentials][:token] })
   end
+
+  def auth_raw_info(value)
+    @auth[:extra][:raw_info].fetch(value, "")
+  end
+
 
   def self.new_with_session(params, session)
     super.tap do |user|
