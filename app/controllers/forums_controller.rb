1494ca308de1b3a0d671bf8d4d9483d2c222e907
Benjamin Hand <gneeshot@gmail.com>
2015-07-17 13:59:45 -0500

    removed respond to blocks from forums controller

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -21,35 +21,24 @@ class ForumsController < ApplicationController
   def create
     @forum = Forum.new(forum_params)
     @forum.user_id = current_user.id
-    respond_to do |format|
-      if @forum.save
-        format.html { redirect_to @forum, notice: 'Forum was successfully created.' }
-        format.json { render :show, status: :created, location: @forum }
-      else
-        format.html { render :new }
-        format.json { render json: @forum.errors, status: :unprocessable_entity }
-      end
+    if @forum.save
+      redirect_to @forum, notice: 'Forum was successfully created.'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @forum.update(forum_params)
-        format.html { redirect_to @forum, notice: 'Forum was successfully updated.' }
-        format.json { render :show, status: :ok, location: @forum }
-      else
-        format.html { render :edit }
-        format.json { render json: @forum.errors, status: :unprocessable_entity }
-      end
+    if @forum.update(forum_params)
+      redirect_to @forum, notice: 'Forum was successfully updated.'
+    else
+      render :edit
     end
   end
 
   def destroy
     @forum.destroy
-    respond_to do |format|
-      format.html { redirect_to forums_url, notice: 'Forum was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to forums_url, notice: 'Forum was successfully destroyed.'
   end
 
   private
