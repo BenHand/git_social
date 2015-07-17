65015447d6c6b0e7c72e32bf35b6eb6f8f172357
Benjamin Hand <gneeshot@gmail.com>
2015-07-17 00:13:45 -0500

    trying another approach with coderay

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,22 @@
 module ApplicationHelper
 
-  class CodeRayify < Redcarpet::Render::HTML
+  class MarkdownRenderer < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.highlight(code, language)
     end
   end
 
   def markdown(text)
-    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
-
-    language ||= :plaintext
-
+    rndr = MarkdownRenderer.new(:filter_html => true, :hard_wrap => true)
     options = {
-              fenced_code_blocks: true,
-              no_intra_emphasis: false,
-              autolink: true,
-              strikethrough: true,
-              lax_html_blocks: true,
-              superscript: true,
-              space_after_headers: true,
-              highlight: true,
-              footnotes: true,
-              }
-
-    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :autolink => true,
+      :strikethrough => true,
+      :lax_html_blocks => true,
+      :superscript => true
+    }
+    markdown_to_html = Redcarpet::Markdown.new(rndr, options)
     markdown_to_html.render(text).html_safe
   end
 
