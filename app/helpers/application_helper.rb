ec3bd3d37ebc5497bf0a302239d124ffb6868b76
Benjamin Hand <gneeshot@gmail.com>
2015-07-16 21:23:06 -0500

    added in markdown with syntax highlighting
    hopefully fixed issue with not being able to login with no public email on github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,27 @@
 module ApplicationHelper
-  def markdown(content)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
-      autolink: true,
-      space_after_headers: true,
-      fenced_code_blocks: true,
-      gh_blockcode: true,
-      underline: true,
-      highlight: true,
-      footnotes: true,
-      no_intra_emphasis: true,
-    })
-    syntax_highlighter(@markdown.render(content)).html_safe
+
+  class HTMLwithPygments < Redcarpet::Render::HTML
+
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+
   end
 
-  def syntax_highlighter(html)
-    doc = Nokogiri::HTML(html)
-    doc.search("//pre[@lang]").each do |pre|
-      pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-    end
-    doc.to_s
+  def markdown(content)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+    options  = {
+                autolink: true,
+                space_after_headers: true,
+                fenced_code_blocks: true,
+                gh_blockcode: true,
+                underline: true,
+                highlight: true,
+                footnotes: true,
+                gh_blockcode: true,
+                no_intra_emphasis: true
+               }
+    Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 
 end
