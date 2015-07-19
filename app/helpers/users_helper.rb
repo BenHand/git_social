c3c252849fef5deb6880096cf38b965b7898fa62
Benjamin Hand <gneeshot@gmail.com>
2015-07-19 13:33:02 -0500

    forgot to end tag on profile activity page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
 
-  def split_url(url)
-    if url.length > 10
-      url.split('/')[-1]
-    else
-      url
-    end
-  end
+  # def split_url(url)
+  #   if url.length > 10
+  #     url.split('/')[-1]
+  #   else
+  #     url
+  #   end
+  # end
 
 end
