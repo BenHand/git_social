5deebf88f193852c0352c6df5630e987fe66b3ce
Benjamin Hand <gneeshot@gmail.com>
2015-07-20 14:43:21 -0500

    re added turbolinks, react footer not working.

---
+++ app/assets/javascripts/components/footer.js.jsx
@@ -0,0 +1,46 @@
+// TODO: finish react component for footer.
+// 'use strict';
+// /* globals React */
+// var Footer = React.createClass({
+//   render: function() {
+//     return (
+//       <footer className="page-footer light-green darken-3">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col l6 s12">
+//               <h5 className="white-text">GitSocial</h5>
+//               <p className="grey-text text-lighten-4">Thanks for dropping by.
+//                 This is a final project for The Iron Yard in Austin, Tx.
+//                 If you have any questions, comments, or suggestions please feel
+//                 free to post them on the forums or checkout the repo.
+//               </p>
+//             </div>
+//             <div className="col l4 offset-l2 s12">
+//               <h5 className="white-text">Links</h5>
+//               <ul>
+//                 <li>
+//                   <a href="https://github.com/BenHand/git_social">Project Repo</a>
+//                 </li>
+//                 <li>
+//                   <a href="https://help.github.com/articles/markdown-basics/">How to markdown</a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//         <div className="footer-copyright">
+//           <div className="container center-text">
+//             <p>
+//               Created by
+//               <a href="https://github.com/BenHand">Benjamin Hand</a>
+//               with a special thanks to
+//               <a href="https://github.com/cczapski">Casie Czapski</a>
+//               <%= link_to "Casie Czapski", "https://github.com/cczapski" %>
+//               for design suggestions.
+//             </p>
+//           </div>
+//         </div>
+//       </footer>
+//       );
+//   }
+// });
